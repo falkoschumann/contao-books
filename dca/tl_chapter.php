494fed8fec8dc67542723d9682b327d319877308
Falko Schumann <falko.schumann@muspellheim.de>
2015-08-23 21:35:14 +0200

    Remove filter from chapter tree view

--- dca/tl_chapter.php
+++ dca/tl_chapter.php
@@ -237,7 +237,6 @@ $GLOBALS['TL_DCA']['tl_chapter'] = array
         (
             'label'     => &$GLOBALS['TL_LANG']['tl_chapter']['published'],
             'exclude'   => true,
-            'filter'    => true,
             'inputType' => 'checkbox',
             'eval'      => array('tl_class' => 'w50'),
             'sql'       => "char(1) NOT NULL default ''"
