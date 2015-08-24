6bda59e28edcfbdcf10ac8e1b934ecdf83cf2788
Falko Schumann <falko.schumann@muspellheim.de>
2015-08-24 21:17:50 +0200

    Fix inserttags

--- classes/BookInsertTags.php
+++ classes/BookInsertTags.php
@@ -24,87 +24,80 @@ namespace Muspellheim\Books;
 class BookInsertTags extends \Frontend
 {
 
-	public function replaceBookInsertTags($strTag)
-	{
-		$arrSplit = explode('::', $strTag);
-		$insertTag = $arrSplit[0];
+    public function replaceBookInsertTags($strTag)
+    {
+        $arrSplit = explode('::', $strTag);
+        $insertTag = $arrSplit[0];
 
-		if ($this->beginsWith($insertTag, 'bookchapter'))
-		{
-			$idOrAlias = $arrSplit[1];
-			$objChapter = $this->getChapter($idOrAlias);
-			if ($objChapter)
-			{
-				$title = $this->getChapterTitle($objChapter);
-				$url = $this->getChapterUrl($objChapter);
-				if ($insertTag == 'bookchapter_open')
-				{
-					return '<a href="' . $url . '" class="bookchapter">';
-				}
-				else if ($insertTag == 'bookchapter_url')
-				{
-					return $this->getChapterUrl($objChapter);
-				}
-				else if ($insertTag == 'bookchapter_title')
-				{
-					return $this->getChapterTitle($objChapter);
-				}
-				else
-				{
-					return '<a href="' . $url . '" class="bookchapter">' . $title . '</a>';
-				}
-			}
-			else if ($insertTag == 'bookchapter_close')
-			{
-				return '</a>';
-			}
-			else
-			{
-				return '{Unbekanntens Kapitel: ' . $idOrAlias . '}';
-			}
-		}
+        if ($this->beginsWith($insertTag, 'bookchapter')) {
+            $idOrAlias = $arrSplit[1];
+            $objChapter = $this->getChapter($idOrAlias);
+            if ($objChapter) {
+                $title = $this->getChapterTitle($objChapter);
+                $url = $this->getChapterUrl($objChapter);
+                if ($insertTag == 'bookchapter_open') {
+                    return '<a href="' . $url . '" class="bookchapter">';
+                } else {
+                    if ($insertTag == 'bookchapter_url') {
+                        return $this->getChapterUrl($objChapter);
+                    } else {
+                        if ($insertTag == 'bookchapter_title') {
+                            return $this->getChapterTitle($objChapter);
+                        } else {
+                            return '<a href="' . $url . '" class="bookchapter">' . $title . '</a>';
+                        }
+                    }
+                }
+            } else {
+                if ($insertTag == 'bookchapter_close') {
+                    return '</a>';
+                } else {
+                    return '{Unbekanntens Kapitel: ' . $idOrAlias . '}';
+                }
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
 
-	function beginsWith($str, $sub)
-	{
-		return (substr($str, 0, strlen($sub)) == $sub);
-	}
+    function beginsWith($str, $sub)
+    {
+        return (substr($str, 0, strlen($sub)) == $sub);
+    }
 
 
-	private function getChapter($idOrAlias)
-	{
-		$objChapters = $this->Database->prepare('SELECT id, title, alias FROM tl_book_chapter WHERE (id=? || alias=?) AND published=1')->execute($idOrAlias, $idOrAlias);
-		return $objChapters->next();
-	}
+    private function getChapter($idOrAlias)
+    {
+        $objChapters = $this->Database->prepare('SELECT id, title, alias FROM tl_chapter WHERE (id=? || alias=?) AND published=1')->execute($idOrAlias, $idOrAlias);
+        return $objChapters->next();
+    }
 
 
-	private function getChapterTitle($objChapter)
-	{
-		$arrHeadline = deserialize($objChapter->title);
-		return is_array($arrHeadline) ? $arrHeadline['value'] : $arrHeadline;
-	}
+    private function getChapterTitle($objChapter)
+    {
+        $arrHeadline = deserialize($objChapter->title);
+        return is_array($arrHeadline) ? $arrHeadline['value'] : $arrHeadline;
+    }
 
 
-	private function getChapterUrl($objChapter)
-	{
-		global $objPage;
-		$page = array(
-			'id'    => $objPage->id,
-			'alias' => $objPage->alias
-		);
+    private function getChapterUrl($objChapter)
+    {
+        global $objPage;
+        $page = array(
+            'id'    => $objPage->id,
+            'alias' => $objPage->alias
+        );
 
-		$itemPrefix = $GLOBALS['TL_CONFIG']['useAutoItem'] ? '/' : '/items/';
-		$item = $this->isAliasSetAndEnabled($objChapter) ? $objChapter->alias : $objChapter->id;
-		return $this->generateFrontendUrl($page, $itemPrefix . $item);
-	}
+        $itemPrefix = $GLOBALS['TL_CONFIG']['useAutoItem'] ? '/' : '/items/';
+        $item = $this->isAliasSetAndEnabled($objChapter) ? $objChapter->alias : $objChapter->id;
+        return $this->generateFrontendUrl($page, $itemPrefix . $item);
+    }
 
 
-	private function isAliasSetAndEnabled($objChapter)
-	{
-		return $objChapter->alias != '' && !$GLOBALS['TL_CONFIG']['disableAlias'];
-	}
+    private function isAliasSetAndEnabled($objChapter)
+    {
+        return $objChapter->alias != '' && !$GLOBALS['TL_CONFIG']['disableAlias'];
+    }
 
 }
--- languages/en/explain.php
+++ languages/en/explain.php
@@ -17,7 +17,7 @@
 $GLOBALS['TL_LANG']['XPL']['bookchapter_text'] = array
 (
 	array('{{bookchapter::*}}', 'Insert a link to a chapter of this book. The star * must be replaced by id or alias of the chapter.'),
-	array('{{bookchapter_open::*}}Click here{{link_close}}', 'Insert a link to a chapter of this book. The star * must be replaced by id or alias of the chapter.'),
+	array('{{bookchapter_open::*}}Click here{{bookchapter_close}}', 'Insert a link to a chapter of this book. The star * must be replaced by id or alias of the chapter.'),
 	array('{{bookchapter_url::*}}', 'Insert the url to a chapter of this book. The star * must be replaced by id or alias of the chapter.'),
 	array('{{bookchapter_title::*}}', 'Insert the title of a chapter of this book. The star * must be replaced by id or alias of the chapter.')
 );
