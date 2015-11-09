89754f0a49ac0817ddf8888679f743ae8341fc3c
Falko Schumann <falko.schumann@muspellheim.de>
2015-11-09 09:12:25 +0100

    Add missing translation for tl_chapter.tstamp

--- dca/tl_chapter.php
+++ dca/tl_chapter.php
@@ -171,7 +171,8 @@ $GLOBALS['TL_DCA']['tl_chapter'] = array
         ),
         'tstamp'    => array
         (
-            'sql' => "int(10) unsigned NOT NULL default '0'"
+            'label' => &$GLOBALS['TL_LANG']['tl_chapter']['tstamp'],
+            'sql'   => "int(10) unsigned NOT NULL default '0'"
         ),
         'title'     => array
         (
